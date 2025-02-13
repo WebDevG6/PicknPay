1d4eb79312f60cb821cb44136c12e2f264db7460
Sirawit Loywirat <sloywirat@gmail.com>
2025-02-13 22:48:08 +0700

    Add Carousel Image and Its function

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,33 +1,41 @@
-import React from "react";
-import { Carousel } from "antd";
+import React, { useState, useRef } from "react";
+import { Carousel, Image } from "antd";
+import conf from "../conf/main";
 
-const contentStyle = {
-    margin: 0,
-    height: "160px",
-    color: "#fff",
-    lineHeight: "160px",
-    textAlign: "center",
-    background: "#364d79",
-};
+function ProductCarousel({ images }) {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const carouselRef = useRef(null);
 
-function ProductCarousel() {
+    const handleCarouselChange = (current) => {
+        setActiveIndex(current);
+    };
+
+    const handleThumbnailClick = (index) => {
+        carouselRef.current.goTo(index);
+        setActiveIndex(index);
+    };
+
     return (
-        <div className="flex flex-col gap-4">
-            <Carousel arrows infinite={false}>
-                <div>
-                    <h3 style={contentStyle}>1</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>2</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>3</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>4</h3>
-                </div>
-            </Carousel>
-            <div>pictures</div>
+        <div className="flex flex-col gap-4 justify-center">
+            <div className="rounded-md shadow-md overflow-hidden">
+                <Carousel ref={carouselRef} arrows afterChange={handleCarouselChange}>
+                    {images.map((image) => (
+                        <Image key={image.url} src={conf.urlPrefix + image.url} />
+                    ))}
+                </Carousel>
+            </div>
+            <div className="grid grid-cols-4 gap-2">
+                {images.map((image, index) => (
+                    <img
+                        key={image.url}
+                        src={conf.urlPrefix + image.url}
+                        className={`col-span-1 cursor-pointer hover:ring-2 ring-[#4169E2] rounded-md transition w-full h-full object-cover shadow-md ${
+                            activeIndex === index ? "ring-2 ring-[#4169E2]" : ""
+                        }`}
+                        onClick={() => handleThumbnailClick(index)}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import ProductCarousel from "../components/ProductCarousel";
 import { useProductDetail } from "../hooks/query";
 import { useParams } from "react-router-dom";
+import { Spin } from "antd";
 
 function Product() {
     const { productId } = useParams();
     const { data: productDetail, isLoading, error } = useProductDetail(productId);
 
     return (
-        <div className="bg-white rounded-md grid grid-cols-2 gap-4 p-4">
-            <div className="col-span-1">
-                <ProductCarousel />
+        <Spin spinning={isLoading}>
+            <div className="bg-white rounded-md grid grid-cols-12 gap-6 p-6">
+                <div className="col-span-5">
+                    <ProductCarousel images={productDetail.picture} />
+                </div>
+                <div className="bg-red-400 col-span-7">{productDetail.name}</div>
             </div>
-            <div className="bg-red-400 col-span-1">{productDetail.name}</div>
-        </div>
+        </Spin>
     );
 }
 
