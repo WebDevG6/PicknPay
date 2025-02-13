9cbea15a9c4480238792f1617e6b2a1e2c32b2e4
Jihan_Hajideramae <6710110065@psu.ac.th>
2025-02-13 13:36:53 +0700

    feat: Add carousel ads

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Carousel from "../components/Carousel";
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -12,7 +13,7 @@ function Home() {
                     { name: "คีย์บอร์ด", image: "https://logitech.e-express.co.th/wp-content/uploads/2024/05/p1.webp", path: "/categories/keyboard" },
                     { name: "ไมโครโฟน", image: "https://mercular.s3.ap-southeast-1.amazonaws.com/images/products/2024/08/Nubwo%20X700.png", path: "/categories/microphone" },
                     { name: "จอคอม", image: "https://i02.appmifile.com/456_item_th/18/03/2024/115d7e9c8cfc2f2d02c6b7064313ffcf!400x400!85.png", path: "/categories/computer" },
-                    { name: "ลำโพง", image: "https://th.jbl.com/dw/image/v2/AAUJ_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw63baedca/JBL_QUANTUM_DUO_HERO_V2_011_Main_x2.png?sw=537&sfrm=png", path: "/categories/speaker" },
+                    { name: "ลำโพง", image: "https://th.jbl.com/dw/image/v2/AAUJ_PRD/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw63baedca/JBL_QUANTUM_DUO_HERO_V2_011_Main_x2.png?sw=537&sfrm=png", path: "/categories/loudspeaker" },
                 ].map((item, index) => (
                     <Link key={index} to={item.path}> {/* คลุม Card ด้วย Link */}
                         <div className="bg-gray-100 p-6 rounded-lg shadow-md flex flex-col items-center cursor-pointer transition-transform hover:scale-105">
@@ -21,6 +22,21 @@ function Home() {
                         </div>
                     </Link>
                 ))}
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-8"> {/* Added mt-8 for margin-top */}
+                <div className="lg:col-span-1 flex flex-col"> {/* Added flex and flex-col */}
+                    {[
+                        { name: "IDK" },
+                    ].map((item, index) => (
+                        <div key={index} className="bg-gray-100 p-20 rounded-lg shadow-md flex flex-col items-center flex-grow"> {/* Added flex-grow */}
+                            <img src={item.image} alt={item.name} className="w-24 h-24 mb-2 object-contain" />
+                            <p className="text-lg font-bold text-blue-700">{item.name}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="lg:col-span-2 flex flex-col "> {/* Added flex and flex-col */}
+                    <Carousel className="flex-grow" /> {/* Added flex-grow */}
+                </div>
             </div>
         </div>
     );
