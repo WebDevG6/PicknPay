3e47c56d5489ca2ef9dbe55502435807cd365b50
Sirawit Loywirat <sloywirat@gmail.com>
2025-02-22 14:51:41 +0700

    hotfix

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -50,7 +50,7 @@ function Product() {
     return (
         <div>
             {contextHolder}
-            <div div className="bg-white rounded-md grid grid-cols-12 gap-6 p-6 z-0" >
+            <div className="bg-white rounded-md grid grid-cols-12 gap-6 p-6 z-0">
                 <div className="col-span-5">
                     <ProductCarousel images={productDetail.picture} />
                 </div>
@@ -114,7 +114,7 @@ function Product() {
                         </div>
                     </div>
                 </div>
-            </div >
+            </div>
         </div>
     );
 }
