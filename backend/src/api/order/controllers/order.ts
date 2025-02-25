2238e384f02f9af902bca487b448d6afe43368fb
Sirawit Loywirat <sloywirat@gmail.com>
2025-02-25 19:59:31 +0700

    hotfix

--- backend/src/api/order/controllers/order.ts
+++ backend/src/api/order/controllers/order.ts
@@ -11,99 +11,90 @@ type order_items = {
     productDocumentId: string;
 };
 
-export default factories.createCoreController(
-    "api::order.order",
-    ({ strapi }) => ({
-        async create(ctx) {
-            const { order_items, couponId } = ctx.request.body;
-            const line_items = await Promise.all(
-                order_items.map(async (product) => {
-                    const item = await strapi
-                        .service("api::product.product")
-                        .findOne(product.productDocumentId);
-                    return {
-                        price_data: {
-                            currency: "thb",
-                            product_data: {
-                                name: item.name,
-                                images: item.imageUrl,
-                            },
-                            unit_amount: item.price * 100,
+export default factories.createCoreController("api::order.order", ({ strapi }) => ({
+    async create(ctx) {
+        const { order_items, couponId } = ctx.request.body;
+        const line_items = await Promise.all(
+            order_items.map(async (product) => {
+                const item = await strapi.service("api::product.product").findOne(product.productDocumentId);
+                return {
+                    price_data: {
+                        currency: "thb",
+                        product_data: {
+                            name: item.name,
+                            images: item.imageUrl,
                         },
-                        quantity: product.quantity,
-                    };
-                })
-            );
-            try {
-                const session = await stripe.checkout.sessions.create({
-                    mode: "payment",
-                    success_url: `${process.env.CLIENT_URL}?success=true`,
-                    cancel_url: `${process.env.CLIENT_URL}?canceled=true`,
-                    line_items: line_items,
-                    discounts: [
-                        {
-                            coupon: couponId,
-                        },
-                    ],
-                    shipping_address_collection: { allowed_countries: ["TH"] },
-                    locale: "th",
-                    customer_email: ctx.state.user.email,
-                    metadata: {
-                        products: JSON.stringify(
-                            order_items.map((product) => ({
-                                productDocumentId: product.productDocumentId,
-                                productQuantity: product.quantity,
-                            }))
-                        ),
-                        cartItems: JSON.stringify(
-                            order_items.map((product) => ({
-                                documentId: product.documentId,
-                            }))
-                        ),
+                        unit_amount: item.price * 100,
                     },
-                    expires_at: Math.floor(Date.now() / 1000) + 1800,
-                });
+                    quantity: product.quantity,
+                };
+            })
+        );
+        try {
+            const session = await stripe.checkout.sessions.create({
+                mode: "payment",
+                success_url: `${process.env.CLIENT_URL}?success=true`,
+                cancel_url: `${process.env.CLIENT_URL}?canceled=true`,
+                line_items: line_items,
+                discounts: couponId ? [{ coupon: couponId }] : [],
+                shipping_address_collection: { allowed_countries: ["TH"] },
+                locale: "th",
+                customer_email: ctx.state.user.email,
+                metadata: {
+                    products: JSON.stringify(
+                        order_items.map((product) => ({
+                            productDocumentId: product.productDocumentId,
+                            productQuantity: product.quantity,
+                        }))
+                    ),
+                    cartItems: JSON.stringify(
+                        order_items.map((product) => ({
+                            documentId: product.documentId,
+                        }))
+                    ),
+                },
+                expires_at: Math.floor(Date.now() / 1000) + 1800,
+            });
 
-                await strapi.service("api::order.order").create({
-                    data: {
-                        order_items: order_items.map((item) => ({
-                            product_id: item.productDocumentId,
-                            quantity: item.quantity,
-                        })),
-                        stripeId: session.id,
-                        customer: ctx.state.user.id,
-                        status_order: "processing",
-                    },
-                });
-                return { stripeSession: session };
-            } catch (err) {
-                console.error(err);
-                ctx.response.status = 500;
-                return err;
-            }
-        },
-        async validateCoupon(ctx) {
-            const { coupon } = ctx.request.body;
+            await strapi.service("api::order.order").create({
+                data: {
+                    order_items: order_items.map((item) => ({
+                        product_id: item.productDocumentId,
+                        quantity: item.quantity,
+                    })),
+                    stripeId: session.id,
+                    customer: ctx.state.user.id,
+                    status_order: "processing",
+                },
+            });
+            return { stripeSession: session };
+        } catch (err) {
+            console.error(err);
+            ctx.response.status = 500;
+            return err;
+        }
+    },
+    async validateCoupon(ctx) {
+        const { coupon } = ctx.request.body;
 
-            if (!coupon) {
-                return ctx.badRequest("Coupon code is required.");
-            }
+        if (!coupon) {
+            return ctx.badRequest("Coupon code is required.");
+        }
 
-            try {
-                const stripeCoupon = await stripe.coupons.retrieve(coupon);
-                console.log(stripeCoupon);
-                return ctx.send({
-                    valid: stripeCoupon.valid,
-                    id: stripeCoupon.id,
-                    amount_off: stripeCoupon.amount_off,
-                    percent_off: stripeCoupon.percent_off,
-                });
-            } catch (error) {
-                return ctx.send({
-                    valid: false,
-                    message: "Invalid or expired coupon.",
-                });
-            }
-        },
-    })
-);
+        try {
+            const stripeCoupon = await stripe.coupons.retrieve(coupon);
+            console.log(stripeCoupon);
+            return ctx.send({
+                valid: stripeCoupon.valid,
+                id: stripeCoupon.id,
+                amount_off: stripeCoupon.amount_off,
+                percent_off: stripeCoupon.percent_off,
+            });
+        } catch (error) {
+            return ctx.send({
+                valid: false,
+                message: "Invalid or expired coupon.",
+            });
+        }
+    },
+}));
