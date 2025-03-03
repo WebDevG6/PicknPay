1a3a58a163eb5c9ddbfc74637cf2eb7d2e4e8311
Sirawit Loywirat <sloywirat@gmail.com>
2025-03-03 12:50:33 +0700

    feat: update carousel images

--- frontend/src/components/public/CarouselHome.jsx
+++ frontend/src/components/public/CarouselHome.jsx
@@ -1,9 +1,8 @@
 import { Carousel } from "antd";
 
 const images = [
-    "https://cdn.mercular.com/images/homepage/sections/8/spaces/11/1736941490411_1000067490.jpg",
     "https://cdn.mercular.com/images/homepage/sections/8/spaces/11/1733912345023_6Headphone-(1).jpg",
-    "https://cdn.mercular.com/images/homepage/sections/8/spaces/11/1710849795305_Mercular_Store_Homepage.jpg",
+    "https://cdn.mercular.com/images/homepage/sections/8/spaces/11/1721029781869_DS_BD_V2-HP.jpg",
 ];
 
 export default function CarouselHome() {
